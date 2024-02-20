d0cb5d6962949833100ac4ca54f64755bfa7c38c
HWEEDONG YOO <yoojon675@gmail.com>
2024-02-20 10:33:10 -0800

    Complete demo07 javascript

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDGee9ixf-4UY_mTTL1DX9hnvJW2YsdhbY",
+    authDomain: "comp1800-demo-cf806.firebaseapp.com",
+    projectId: "comp1800-demo-cf806",
+    storageBucket: "comp1800-demo-cf806.appspot.com",
+    messagingSenderId: "221083370510",
+    appId: "1:221083370510:web:74ab4254e790e36c66685f"
 };
 
 //--------------------------------------------
